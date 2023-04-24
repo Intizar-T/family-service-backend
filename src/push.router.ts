c0478140307681b61eaaa1ab9f7f767257e7a965
Intizar Tashov <intizar.tashov22@gmail.com>
2023-04-24 11:55:27 +0300

    added vitamins reminder

--- src/push.router.ts
+++ src/push.router.ts
@@ -18,9 +18,9 @@ const USER_LAMBDA_URL =
   "https://z7x9jyq1mj.execute-api.ap-northeast-2.amazonaws.com/dev/user";
 
 interface PushParams {
-  userId: string;
-  name: string;
-  message: string;
+  userId?: string;
+  message?: string;
+  vitamins?: boolean;
 }
 
 type APIUsers = {
@@ -44,45 +44,61 @@ type APIUsers = {
 const DEFAULT_FAIL_MESSAGE = "Shulara uwedomleniya iwarip bolmady: ";
 
 push.post("/", async (req: Request, res: Response) => {
-  const { userId, message }: PushParams = req.body;
+  const { userId, message, vitamins }: PushParams = req.body;
   try {
     const usersData = await fetch(USER_LAMBDA_URL, {
       method: "GET",
     });
     const users: APIUsers[] = await usersData.json();
-    let senderSub = "";
-    const subscriptionsRecord = users
-      .filter(({ id, subscription }) => {
-        if (id["N"] === userId) senderSub = subscription?.S;
-        return id["N"] !== userId && subscription?.S;
-      })
-      .map(({ subscription, name }) => {
-        return {
-          sub: subscription.S,
-          name,
-        };
-      });
-    let failMessage = DEFAULT_FAIL_MESSAGE;
-    for (const { sub, name } of subscriptionsRecord) {
-      const result: webpush.SendResult = await webpush.sendNotification(
-        JSON.parse(sub),
-        JSON.stringify({
-          message,
+
+    if (vitamins == null) {
+      let senderSub = "";
+      const subscriptionsRecord = users
+        .filter(({ id, subscription }) => {
+          if (id["N"] === userId) senderSub = subscription?.S;
+          return id["N"] !== userId && subscription?.S;
         })
+        .map(({ subscription, name }) => {
+          return {
+            sub: subscription.S,
+            name,
+          };
+        });
+      let failMessage = DEFAULT_FAIL_MESSAGE;
+      for (const { sub, name } of subscriptionsRecord) {
+        const result: webpush.SendResult = await webpush.sendNotification(
+          JSON.parse(sub),
+          JSON.stringify({
+            message,
+          })
+        );
+        console.log(result.statusCode);
+        if (result.statusCode === 410) {
+          failMessage += `${name}`;
+        }
+      }
+      if (failMessage !== "Shulara uwedomleniya iwarip bolmady: ") {
+        await webpush.sendNotification(
+          JSON.parse(senderSub),
+          JSON.stringify({ message: failMessage })
+        );
+      }
+      return res.status(200).send({ success: true });
+    } else {
+      console.log("vitamins");
+      const subscriptionsRecord = users.map(
+        ({ subscription }) => subscription?.S
       );
-      console.log(result.statusCode);
-      if (result.statusCode === 410) {
-        failMessage += `${name}`;
+      for (const subscription of subscriptionsRecord) {
+        const result: webpush.SendResult = await webpush.sendNotification(
+          JSON.parse(subscription),
+          JSON.stringify({
+            message,
+          })
+        );
+        console.log(result.statusCode);
       }
     }
-    console.log(failMessage);
-    if (failMessage !== "Shulara uwedomleniya iwarip bolmady: ") {
-      await webpush.sendNotification(
-        JSON.parse(senderSub),
-        JSON.stringify({ message: failMessage })
-      );
-    }
-    return res.status(200).send({ success: true });
   } catch (error) {
     console.log(error);
     res.status(500).send(error.message);
